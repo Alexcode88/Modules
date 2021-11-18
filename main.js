b28d224e235329991f703f7d80bf34bd8579a833
Alfredo08 <alfredosv08@gmail.com>
2021-11-18 11:22:19 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const {add, multiply} = require( './math' );
+
+let num1 = 20;
+let num2 = 30;
+
+let result = add( num1, num2 );
+
+console.log( result );
+
+result = multiply( num1, num2 );
+
+console.log( result );
---
+++ math.js
@@ -0,0 +1,13 @@
+
+function addTwoNums( n1, n2 ){
+    return n1 + n2;
+}
+
+function multiplyTwoNums( n1, n2 ){
+    return n1 * n2;
+}
+
+module.exports = {
+    add: addTwoNums,
+    multiply: multiplyTwoNums
+};
